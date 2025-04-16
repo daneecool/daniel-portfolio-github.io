9c6729bd71e20082da88f7cd3ba32382006f19f6
daneecool <daneereiji@gmail.com>
2025-04-16 12:29:40 +0900

    deploy01

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,20 @@ import mdx from "@next/mdx";
 
 const withMDX = mdx({
   extension: /\.mdx?$/,
-  options: {},
+  options: {}, // Add MDX-specific options if needed
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
+  output: "export", // Ensures static files are generated
+  basePath: "/portfolio", // Replace with your repository name if deploying to a project page
+  assetPrefix: "/portfolio", // Replace with your repository name if deploying to a project page
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint during builds
+  },
 };
 
 export default withMDX(nextConfig);
+
+
