d848096224378988a7289bbc80f6349b40be6df1
daneecool <daneereiji@gmail.com>
2025-04-16 15:17:24 +0900

    test

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -1,6 +1,6 @@
 import { InlineCode } from "@/once-ui/components";
 // import { baseURL } from "./config";
-const baseURL = "daneecool.github.io/daniel-portfolio-github.io"; // Replace with your actual base URL
+const baseURL = "https://daneecool.github.io/daniel-portfolio-github.io"; // Ensure the full URL is used
 
 const person = {
   firstName: "Goh",
