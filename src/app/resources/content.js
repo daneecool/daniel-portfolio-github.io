8b3a0faee9bf37f462d0afab264a6c89084575d0
daneecool <daneereiji@gmail.com>
2025-04-16 13:50:48 +0900

    image bug

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -7,7 +7,7 @@ const person = {
     return `${this.firstName} ${this.lastName}`;
   },
   role: "Front-End Engineer",
-  avatar: "public/images/avatar.jpg",
+  avatar: "/daniel-portfolio-github.io/images/avatar.jpg",
   location: "Asia/Tokyo", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
   languages: ["English", "Bahasa Melayu", "Japanese", "Chinese"], // optional: Leave the array empty if you don't want to display languages
 };
